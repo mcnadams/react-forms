c69c7cb4979bbc2c4a4b305bc70dbdf231892cc7
mcnadams <mcneilbo@oregonstate.edu>
2019-05-15 15:16:04 -0700

    refactor input change handlers

--- src/components/TextFormatter.js
+++ src/components/TextFormatter.js
@@ -11,21 +11,17 @@ export default class TextFormatter extends PureComponent {
     this.setState({ formatted: this.state.input + '!' });
   }
 
-  handleInputChange = ({ target }) => {
-    this.setState({ input: target.value }, () => {
+  handleChange = ({ target }) => {
+    this.setState({ [target.name]: target.value }, () => {
       this.formatInput();
     });
-  }
-
-  handleColorChange = ({ target }) => {
-    this.setState({ color: target.value });
   }
 
   render() {
     return (
       <>
-        <input name='input' value={this.state.input} onChange={this.handleInputChange} />
-        <input name='color' type='color' value={this.state.color} onChange={this.handleColorChange} />
+        <input name='input' value={this.state.input} onChange={this.handleChange} />
+        <input name='color' type='color' value={this.state.color} onChange={this.handleChange} />
         <div style={{ color: this.state.color }}>{this.state.formatted}</div>
       </>
     );
