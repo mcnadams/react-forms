d28d8c512898cc078cacace1edbd2a14a7f8c8ae
mcnadams <mcneilbo@oregonstate.edu>
2019-05-16 10:18:59 -0700

    calculate rgb

--- src/components/Color.js
+++ src/components/Color.js
@@ -3,16 +3,20 @@ import PropTypes from 'prop-types';
 
 function Color({ name, hex }) {
 
+  const decimalFromHex = hexSlice => {
+    return Number.parseInt(`0x${hexSlice}`, 16);
+  };
+
   const rgb = {
-    red: '',
-    green: '',
-    blue: ''
+    red: decimalFromHex(hex.slice(1, 3)),
+    green: decimalFromHex(hex.slice(3, 5)),
+    blue: decimalFromHex(hex.slice(5, 7))
   };
 
   return (
     <dl>
-      {name && <><dt>Name:</dt>
-      <dd>{name}</dd></>}
+      <dt>Name:</dt>
+      <dd>{name}</dd>
 
       <dt>Hex:</dt>
       <dd>{hex}</dd>
@@ -23,11 +27,11 @@ function Color({ name, hex }) {
       <dd>Blue: {rgb.blue}</dd>      
     </dl>
   );
-  
+
 }
 
 Color.propTypes = {
-  name: PropTypes.string,
+  name: PropTypes.string.isRequired,
   hex: PropTypes.string.isRequired
 };
 
