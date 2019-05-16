4ef36cf7de8ddc121a9084aaef4ec27ffd403003
mcnadams <mcneilbo@oregonstate.edu>
2019-05-15 20:24:06 -0700

    fix color key

--- src/components/Colors.js
+++ src/components/Colors.js
@@ -5,7 +5,7 @@ import Color from './Color';
 function Colors({ colors }) {
   const colorList = colors.map(color => {
     return (
-      <li key='color.name'>
+      <li key={color.name}>
         <Color name={color.name} hex={color.color} />
       </li>
     );
